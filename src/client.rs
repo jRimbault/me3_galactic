b8b725e3a9ca5a04f71fa0d2f5e5be8b5e9b1802
jRimbault <jacques.rimbault@gmail.com>
2020-08-28 22:05:32 +0200

    extract method

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+const REDIRECTED_URL_BOUND: usize = 27;
+
 #[derive(Debug)]
 pub struct N7Client {
     client: reqwest::blocking::Client,
@@ -41,26 +43,34 @@ impl N7Client {
         M: Into<Mission>,
         I: IntoIterator<Item = M>,
     {
-        let redirected_url: &str = &super::BASE_URL[..27];
-        missions.into_iter().map(Into::into).map(move |mission| {
-            self.client
-                .post(super::BASE_URL)
-                .query(&[("ajax_action", mission.action.value())])
-                .form(&[("mission_code", &mission.name)])
-                .send()
-                .map_err(|e| e.into())
-                .and_then(|r| {
-                    if r.status() != 200 {
-                        Err(anyhow::anyhow!("unknown, {}", r.status())
-                            .context(format!("failed {} for {}", mission.action, mission.name)))
-                    } else if r.url().as_str() == redirected_url {
-                        Err(anyhow::anyhow!("cookie might be expired")
-                            .context(format!("failed {} for {}", mission.action, mission.name)))
-                    } else {
-                        r.json().map_err(Into::into)
-                    }
-                })
-        })
+        missions
+            .into_iter()
+            .map(move |mission| self.launch_mission(mission))
+    }
+
+    fn launch_mission<M>(&self, mission: M) -> anyhow::Result<super::N7Response>
+    where
+        M: Into<Mission>,
+    {
+        let redirected_url = &super::BASE_URL[..REDIRECTED_URL_BOUND];
+        let mission = mission.into();
+        self.client
+            .post(super::BASE_URL)
+            .query(&[("ajax_action", mission.action.value())])
+            .form(&[("mission_code", &mission.name)])
+            .send()
+            .map_err(Into::into)
+            .and_then(|r| {
+                if r.status() != 200 {
+                    Err(anyhow::anyhow!("unknown, {}", r.status())
+                        .context(format!("failed {} for {}", mission.action, mission.name)))
+                } else if r.url().as_str() == redirected_url {
+                    Err(anyhow::anyhow!("cookie might be expired")
+                        .context(format!("failed {} for {}", mission.action, mission.name)))
+                } else {
+                    r.json().map_err(Into::into)
+                }
+            })
     }
 }
 
