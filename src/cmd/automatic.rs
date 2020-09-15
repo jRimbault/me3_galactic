77ac0fac3915db57a2a0a9e97e991a615eeec6b4
jRimbault <jacques.rimbault@gmail.com>
2020-09-15 18:35:35 +0200

    feat: add daemonize mode
    a bit clunky

--- src/cmd/automatic.rs
+++ src/cmd/automatic.rs
@@ -6,10 +6,19 @@ use structopt::StructOpt;
 pub struct Automatic {
     #[structopt(flatten)]
     cookie: super::N7Cookie,
+    /// run in the background automatically
+    #[structopt(short, long)]
+    daemonize: bool,
+    /// for daemon mode, default will not to log
+    #[structopt(short, long, parse(from_os_str))]
+    log_file: Option<std::path::PathBuf>,
 }
 
 impl Automatic {
     pub fn run(self) {
+        if self.daemonize {
+            self.daemonize();
+        }
         let client = Client::with_cookie(&self.cookie.value);
         while let Some(duration_left) = inner_loop(&client) {
             log::info!(
@@ -19,6 +28,24 @@ impl Automatic {
             std::thread::sleep(duration_left);
         }
         std::process::exit(1);
+    }
+
+    fn daemonize(self) -> ! {
+        let program: std::path::PathBuf = std::env::args_os().next().unwrap().into();
+        let mut cmd = std::process::Command::new(program);
+        cmd.env(crate::ID_COOKIE, &self.cookie.value)
+            .arg("automatic");
+        if let Some(log_file) = self.log_file.as_deref() {
+            cmd.stderr(std::fs::File::create(&log_file).unwrap());
+        }
+        // not waiting on it, just dropping it for the OS to maybe pick up later..
+        match cmd.spawn() {
+            Err(error) => {
+                log::error!("couldn't launch program, {:#}", anyhow::Error::from(error));
+                std::process::exit(1);
+            }
+            Ok(_) => std::process::exit(0),
+        }
     }
 }
 
